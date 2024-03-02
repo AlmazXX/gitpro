c7eefea55cc0220361ae3f4ee15ac00ea3f27d5a
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-02 20:37:40 +0600

    prep: Added assets

---
+++ client/src/components/icons/index.ts
@@ -0,0 +1,6 @@
+export { GitHub } from './GitHub';
+export { Location } from './Location';
+export { Email } from './Email';
+export { Url } from './Url';
+export { Follows } from './Follows';
+export { Company } from './Company';
