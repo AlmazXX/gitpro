9c11e25c5b878a1682f5a91e17a089b10084a943
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-03 00:08:55 +0600

    prep: Added icons

--- client/src/components/icons/index.ts
+++ client/src/components/icons/index.ts
@@ -4,3 +4,5 @@ export { Email } from './Email';
 export { Url } from './Url';
 export { Follows } from './Follows';
 export { Company } from './Company';
+export { Repository } from './Repository';
+export { Edit } from './Edit';
