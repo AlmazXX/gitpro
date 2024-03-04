5bf6f851c71e0154b3fdd34cce682270f158bb61
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-05 00:03:44 +0600

    prep: Changed main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import './index.css';
 import store from './store/store.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.createRoot(root).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
