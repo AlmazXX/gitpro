dd80655dfc9217cd6d4d94e0cfbb5b4045d09102
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-05 00:05:38 +0600

    feat: Separated user handling to service and route

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,70 +1,17 @@
-import axios from "axios";
 import { Router } from "express";
-import { axiosApi } from "../axiosApi.js";
-import { CLIENT_ID, CLIENT_SECRET } from "../config/config.js";
-import {
-  GITHUB_ACCESS_TOKEN,
-  GITHUB_ACCESS_TOKEN_REQUEST_URL,
-} from "../constants.js";
+import UserService from "../services/users.js";
 
 const userRouter = Router();
+const userService = new UserService();
 
-userRouter.get("/callback", async (req, res, next) => {
-  try {
-    const { code } = req.query;
-    const gitUrl = new URL(GITHUB_ACCESS_TOKEN_REQUEST_URL);
-    const gitQuery = new URLSearchParams({
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      code,
-    });
-    gitUrl.search = gitQuery;
+userRouter.get("/callback", userService.authorize);
 
-    const response = await axios.post(gitUrl);
-    const { data } = response;
+userRouter.get("/me", userService.getSelf);
 
-    const access_token = new URLSearchParams(data).get(GITHUB_ACCESS_TOKEN);
+userRouter.patch("/me", userService.updateSelf);
 
-    res
-      .status(204)
-      .cookie(GITHUB_ACCESS_TOKEN, access_token, {
-        sameSite: "strict",
-        maxAge: new Date().getTime() + 5 * 1000,
-        path: "/",
-      })
-      .redirect("http://localhost:5173/app");
+userRouter.delete("/me", userService.unauthorize);
 
-    return res.end();
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-userRouter.get("/me", async (req, res, next) => {
-  try {
-    const { access_token } = req.cookies;
-
-    if (!Boolean(access_token)) {
-      return res.status(401).send({ error: "Unauthorized" });
-    }
-
-    const response = await axiosApi.get("/user", {
-      headers: { Authorization: `Bearer ${access_token}` },
-    });
-
-    const data = response.data;
-    return res.status(200).send(data);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-userRouter.delete("/me", (_req, res, next) => {
-  try {
-    res.status(204).clearCookie(GITHUB_ACCESS_TOKEN);
-  } catch (error) {
-    console.log(error);
-  }
-});
+userRouter.get("/search", userService.searchUsers);
 
 export default userRouter;
---
+++ api/src/services/users.js
@@ -0,0 +1,156 @@
+import axios, { isAxiosError } from "axios";
+import { ApiError } from "../ApiError.js";
+import { axiosApi } from "../axiosApi.js";
+import { CLIENT_ID, CLIENT_SECRET } from "../config/config.js";
+import {
+  GITHUB_ACCESS_TOKEN,
+  GITHUB_ACCESS_TOKEN_REQUEST_URL,
+} from "../constants.js";
+
+class UserService {
+  constructor() {}
+
+  async authorize(req, res) {
+    try {
+      const { code } = req.query;
+      const gitUrl = new URL(GITHUB_ACCESS_TOKEN_REQUEST_URL);
+      const gitQuery = new URLSearchParams({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code,
+      });
+      gitUrl.search = gitQuery;
+
+      const response = await axios.post(gitUrl);
+      const { data } = response;
+
+      const access_token = new URLSearchParams(data).get(GITHUB_ACCESS_TOKEN);
+
+      res
+        .status(204)
+        .cookie(GITHUB_ACCESS_TOKEN, access_token, {
+          sameSite: "strict",
+          maxAge: new Date().getTime() + 5 * 1000,
+          path: "/",
+        })
+        .redirect("http://localhost:5173/app");
+      return res.end();
+    } catch (error) {
+      if (isAxiosError(error) && error.response && error.response.status) {
+        return res
+          .status(error.response.status)
+          .send({ message: error.response.data.message });
+      }
+      return res.status(error.httpCode).send({ message: error.message });
+    }
+  }
+
+  unauthorize(_, res) {
+    try {
+      res.status(204).clearCookie(GITHUB_ACCESS_TOKEN);
+      return res.end();
+    } catch (error) {
+      if (isAxiosError(error) && error.response && error.response.status) {
+        return res
+          .status(error.response.status)
+          .send({ message: error.response.data.message });
+      }
+      return res.status(error.httpCode).send({ message: error.message });
+    }
+  }
+
+  async getSelf(req, res) {
+    try {
+      const { access_token } = req.cookies;
+
+      if (!Boolean(access_token)) {
+        throw new ApiError("Unauthorized", 401);
+      }
+
+      const { data } = await axiosApi.get("/user", {
+        headers: { Authorization: `Bearer ${access_token}` },
+      });
+
+      const user = {
+        name: data.name,
+        login: data.login,
+        email: data.email || "",
+        avatar_url: data.avatar_url || "",
+        gravatar_id: data.avatar_url,
+        company: data.company || "",
+        location: data.location || "",
+        bio: data.bio || "",
+        html_url: data.html_url,
+        followers_url: data.followers_url,
+        following_url: data.following_url,
+        followers: data.followers,
+        following: data.following,
+      };
+
+      return res.status(200).send(user);
+    } catch (error) {
+      if (isAxiosError(error) && error.response && error.response.status) {
+        return res
+          .status(error.response.status)
+          .send({ message: error.response.data.message });
+      }
+      return res.status(error.httpCode).send({ message: error.message });
+    }
+  }
+
+  async updateSelf(req, res) {
+    try {
+      const { access_token } = req.cookies;
+
+      if (!Boolean(access_token)) {
+        throw new ApiError("Unauthorized", 401);
+      }
+
+      const { body } = req;
+
+      await axiosApi.patch("/user", body, {
+        headers: { Authorization: `Bearer ${access_token}` },
+      });
+
+      return res.send("OK");
+    } catch (error) {
+      if (isAxiosError(error) && error.response && error.response.status) {
+        return res
+          .status(error.response.status)
+          .send({ message: error.response.data.message });
+      }
+      return res.status(error.httpCode).send({ message: error.message });
+    }
+  }
+
+  async searchUsers(req, res) {
+    try {
+      const { user } = req.query;
+
+      if (!Boolean(user)) {
+        throw new ApiError("User name is required", 406);
+      }
+
+      const { data } = await axiosApi.get(`search/users?q=${user}+in:fullname`);
+
+      const users = data.items.map((user) => ({
+        login: user.login,
+        avatar_url: user.avatar_url || "",
+        gravatar_id: user.avatar_url,
+        html_url: user.html_url,
+        repos_url: user.repos_url,
+      }));
+
+      return res.send({ users, total_count: data.total_count });
+    } catch (error) {
+      if (isAxiosError(error) && error.response && error.response.status) {
+        return res
+          .status(error.response.status)
+          .send({ message: error.response.data.message });
+      }
+      return res.status(error.httpCode).send({ message: error.message });
+    }
+  }
+}
+
+export default UserService;
