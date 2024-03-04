17974ef2bf220f4ea526caa71bec2b5af2ad1de0
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-04 23:51:18 +0600

    feat: Configured axios

---
+++ client/src/axiosApi.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { BASE_URL } from './constants';
+
+const axiosApi = axios.create({
+  baseURL: BASE_URL,
+  responseType: 'json',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+export default axiosApi;
