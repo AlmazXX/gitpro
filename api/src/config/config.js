237f217d019e4d97ef4f22ac0fc1c80be5fcc2e1
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-20 14:04:53 +0600

    fix: Added dotenv for reading env variables

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const EXPRESS_PORT = process.env.EXPRESS_PORT;
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
