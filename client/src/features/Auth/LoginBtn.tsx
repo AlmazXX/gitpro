24aa92abe71fd4e890e4631b90354a09688118a5
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-20 14:53:18 +0600

    tmp: Replaced button with a tag for gh button

--- client/src/features/Auth/LoginBtn.tsx
+++ client/src/features/Auth/LoginBtn.tsx
@@ -2,19 +2,20 @@ import { GitHub } from '../../components/icons';
 import { GITHUB_OAUTH_LINK } from '../../constants';
 import styles from './Auth.module.css';
 
-function loginWithGithub() {
-  window.location.assign(GITHUB_OAUTH_LINK);
-}
+// function loginWithGithub() {
+//   console.log(GITHUB_OAUTH_LINK);
+//   window.location.href = GITHUB_OAUTH_LINK;
+// }
 
 export const LoginBtn = () => {
   return (
     <span className={styles.auth__gh_wrap}>
-      <button className={styles.uauth__gh_btn} onClick={loginWithGithub}>
+      <a className={styles.auth__gh_btn} href={GITHUB_OAUTH_LINK}>
         <span className={styles.auth__gh_icon}>
           <GitHub />
         </span>
         <span>Sign up with GitHub</span>
-      </button>
+      </a>
     </span>
   );
 };
