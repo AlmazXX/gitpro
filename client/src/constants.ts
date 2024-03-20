46dac700fc6272b59e545cb4ec87b3d6db7ca3a2
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-03-20 21:30:03 +0600

    Canceled last fix of client components

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,3 @@
-export const GITHUB_OAUTH_LINK = `https://github.com/login/oauth/authorize?scope=user%2Crepo&client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}`;
-export const GITHUB_ACCESS_TOKEN = 'access_token';
 export const BASE_URL = 'http://localhost:8626/api';
+export const GITHUB_OAUTH_LINK = `https://github.com/login/oauth/authorize?scope=user,repo&client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}&redirect_uri=${BASE_URL}/users/callback`;
+export const GITHUB_ACCESS_TOKEN = 'access_token';
--- client/src/features/Auth/LoginBtn.tsx
+++ client/src/features/Auth/LoginBtn.tsx
@@ -2,20 +2,19 @@ import { GitHub } from '../../components/icons';
 import { GITHUB_OAUTH_LINK } from '../../constants';
 import styles from './Auth.module.css';
 
-// function loginWithGithub() {
-//   console.log(GITHUB_OAUTH_LINK);
-//   window.location.href = GITHUB_OAUTH_LINK;
-// }
+function loginWithGithub() {
+  window.location.assign(GITHUB_OAUTH_LINK);
+}
 
 export const LoginBtn = () => {
   return (
     <span className={styles.auth__gh_wrap}>
-      <a className={styles.auth__gh_btn} href={GITHUB_OAUTH_LINK}>
+      <button className={styles.auth__gh_btn} onClick={loginWithGithub}>
         <span className={styles.auth__gh_icon}>
           <GitHub />
         </span>
         <span>Sign up with GitHub</span>
-      </a>
+      </button>
     </span>
   );
 };
